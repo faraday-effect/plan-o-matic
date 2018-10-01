720ab0a01c05cd35579e298edae7696139f55182
Tom Nurkkala <tnurkkala@cse.taylor.edu>
2018-10-01 00:05:06 -0400

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+    <div id="app">
+        <img alt="Vue logo" src="./assets/logo.png">
+        <MyCircle/>
+        <HelloWorld msg="plan-o-matic"/>
+    </div>
+</template>
+
+<script>
+    import HelloWorld from './components/HelloWorld.vue';
+    import MyCircle from './components/MyCircle.vue';
+
+    export default {
+        name: 'app',
+        components: {
+            HelloWorld,
+            MyCircle
+        }
+    }
+</script>
+
+<style>
+    #app {
+        font-family: 'Avenir', Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: #2c3e50;
+        margin-top: 60px;
+    }
+</style>
---
+++ src/components/MyCircle.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <p>Hello, Circle!</p>
+        <div id="round-stuff"></div>
+    </div>
+</template>
+
+<script>
+    import SVG from 'svg.js';
+
+    export default {
+        name: "MyCircle",
+        mounted: function () {
+            let draw = SVG('round-stuff').size(300, 300);
+            draw.rect(300, 300).attr({ fill: null, stroke: '#f06'});
+            draw.rect(100, 100).attr({ fill: '#f06' });
+            draw.circle(42).attr({fill: '#97c4ff'});
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
