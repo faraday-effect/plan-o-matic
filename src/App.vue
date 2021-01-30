527a3edcca128d794809227258bb1e310f716dff
Tom Nurkkala <tnurkkala@cse.taylor.edu>
2021-01-30 11:41:45 -0500

    Pending changes

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,22 @@
 <template>
-    <div id="app" class="container">
-        <div class="columns">
-            <div class="column">
-                <Plan/>
-            </div>
-        </div>
+  <div id="app" class="container">
+    <div class="columns">
+      <div class="column">
+        <Plan />
+      </div>
     </div>
+  </div>
 </template>
 
 <script>
-    import Plan from './components/SemesterPlan.vue';
+import Plan from "./components/SemesterPlan.vue";
 
-    export default {
-        name: 'app',
-        components: {
-            Plan
-        }
-    }
+export default {
+  name: "app",
+  components: {
+    Plan
+  }
+};
 </script>
 
-<style>
-</style>
+<style></style>
--- src/main.js
+++ src/main.js
@@ -1,12 +1,11 @@
-import Vue from 'vue'
-import App from './App.vue'
-import 'bulma/css/bulma.css'
+import Vue from "vue";
+import App from "./App.vue";
+import "bulma/css/bulma.css";
 
-Vue.filter('moment', m => m ? m.format('ddd/DD-MMM') : '');
+Vue.filter("moment", m => (m ? m.format("ddd/DD-MMM") : ""));
 
 Vue.config.productionTip = false;
 
 new Vue({
-    render: h => h(App)
-}).$mount('#app');
-
+  render: h => h(App)
+}).$mount("#app");
